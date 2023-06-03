59634f621c9f388b5339ac653b9b42668c996840
saneyoshi <aaa0410as@gmail.com>
2023-06-03 21:30:04 +0900

    add:auth page, textfield create.

---
+++ app/(userAuth)/_components/AuthForm.tsx
@@ -0,0 +1,97 @@
+'use client';
+
+import ExternalTextField from '@/app/_components/textField/ExternalTextField';
+import { useCallback, useState } from 'react';
+import {
+  FieldValues,
+  SubmitHandler,
+  useForm
+} from 'react-hook-form';
+
+type Variant = 'LOGIN' | 'REGISTER';
+
+const AuthForm = () => {
+  const [variant, setVariant] = useState<Variant>('LOGIN');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(
+    () => {
+      if (variant === 'LOGIN') {
+        setVariant('REGISTER');
+      } else {
+        setVariant('LOGIN');
+      }
+    },
+    [variant],
+  );
+
+  const {
+    register,
+    handleSubmit,
+    formState: {
+      errors
+    }
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: ''
+    }
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    if (variant === 'REGISTER') {
+      
+    }
+
+    if (variant === 'LOGIN') {
+
+    }
+  }
+
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+
+    // NextAuth Social Sign In
+  }
+
+  return (
+    <div
+      className="
+        mt-8
+        sm:mx-auto
+        sm:w-full
+        sm:max-w-md
+      "
+    >
+      <div
+        className="
+          bg-white
+          px-4
+          py-8
+          shadow
+          sm:rounded-lg
+          sm:px-10
+        "
+      >
+        <form
+          className="
+            space-y-6
+          "
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <ExternalTextField
+            id="email"
+            label="Email"
+            register={register}
+            errors={errors}
+          />
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default AuthForm;
